17cc206f63ff83383a38b482751f802ea378c461
naimish <naimishbhesaniya212@gmail.com>
2023-01-07 22:46:01 +0530

    REdux-toolkit demo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <div>
+
     <div>Home</div>
+    <p> Visit the product page,
+       and you can add or remove product form cart, and  you can also
+        sow your card total amount with tex and widthout tex</p>
+
+    </div>
   )
 }
 
