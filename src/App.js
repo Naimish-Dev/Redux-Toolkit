fb3fea1c39cea2a25cb657acc004b3e07446fa44
Chetan Jograjiya <102139557+chetanj99@users.noreply.github.com>
2024-02-02 08:53:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
-import NavBar from './Cmponent/NavBar'
-import Products from './pages/Products'
-import Account from './pages/Account'
-import { Provider } from 'react-redux'
-import Store from './Redux/Store'
-const App = () => {
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Products from "./pages/Products";
+import Account from "./pages/Account";
+import Store from "./Redux/Store";
+import NavBar from "./Cmponent/NavBar";
+
+const App = () => {
   return (
     <Provider store={Store}>
-    <BrowserRouter>
-    <NavBar/>
-    <Routes>
-      <Route path='/' element={<Home/>} ></Route>
-      <Route path='/products' element={<Products/>} ></Route>
-      <Route path='/cart'  element={<Cart/> }></Route>
-      <Route path='/account' element={<Account/>} ></Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/account" element={<Account />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
